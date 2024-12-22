b7973328ceb17ff46942d7d7ce7926b509ac45dd
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-12-22 21:58:38 +0545

    added routing and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Corrected the typo
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signout from './pages/Signup';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div  className='text-red-500'>App</div>
-  )
+    <BrowserRouter> {/* Corrected component name */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
