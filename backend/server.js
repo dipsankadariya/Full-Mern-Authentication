04f446d94c57616c33372fbd8faa2dc8d1b1d388
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-12-24 14:01:03 +0545

    added signup api and hashing

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 const app = express()
-
+app.use(express.json());
 
 
 mongoose.connect(process.env.MONGO_URI)
@@ -22,4 +23,5 @@ app.listen(3000,()=>{
 
 
 //routes
-app.use('/api/user',userRoutes)
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
