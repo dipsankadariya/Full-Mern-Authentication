a7bd59bda35f98386526b46a77d37a3aa1f2b589
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-12-24 20:09:38 +0545

    added middleware and functions to handle errors

--- backend/utils/error.js
+++ backend/utils/error.js
@@ -1,6 +1,8 @@
+
+//this is a utility function to create and return a custom error object
 export const errorHandler =(statusCode,message)=>{
-    const error = new Error();
-    error.statusCode =statusCode;
+    const error = new Error(); 
+    error.statusCode =statusCode; 
     error.message =message;
     return error;
 }
