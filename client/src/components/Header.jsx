ddfab225eb347cb24c1753c7e14c9e7baf6701ab
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-12-24 20:47:05 +0545

    added Signup Ui page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div className='bg-blue-800 py-4 px-4'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <h1 className='font-bold text-white'>Mern Authentication Boiler Plate</h1>
-            <ul className='flex gap-4 text-white'>
-             <Link to='/'> <li>Home</li></Link>  
-             <Link to='/about'> <li>About</li></Link>  
-             <Link to='/sign-in'> <li>SignIn</li></Link>  
-             
-            </ul>
-        </div>
+    <div className="bg-black text-white">
+      <div className="flex justify-between items-center max-w-6xl mx-auto py-6 px-3">
+        <h1 className="font-semibold text-white text-lg">
+          MERN Auth Boilerplate
+        </h1>
+        <ul className="flex gap-6 text-lg">
+          <Link to="/" className="hover:underline">
+            <li className="transition-all">Home</li>
+          </Link>
+          <Link to="/about" className="hover:underline">
+            <li className="transition-all">About</li>
+          </Link>
+          <Link to="/sign-up" className="hover:underline">
+            <li className="transition-all">SignUp</li>
+          </Link>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='font-bold text-3xl text-center my-8'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input 
+        type='text' 
+        placeholder='Username'
+        id='username'
+        className='bg-gray-200 rounded-lg p-3 text-left text-lg'
+        />
+          <input 
+        type='text' 
+        placeholder='Email'
+        id='email'
+        className='bg-gray-200 rounded-lg p-3 text-left text-lg'
+        />
+          <input 
+        type='text' 
+        placeholder='Password'
+        id='password'
+        className='bg-gray-200 rounded-lg p-3 text-left text-lg'
+        />
+        <button type='submit'className='bg-black py-3 text-white text-lg hover:bg-gray-800'>Sign Up</button>
+       </form>
+    </div>
   )
 }
 
