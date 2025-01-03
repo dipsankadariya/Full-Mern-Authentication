2676f59c18e4199c618c58f0a638fe590c051226
Dipsan Kadariya <dipsankadariya99@gmail.com>
2025-01-03 14:39:55 +0545

    update headercomp,make profile  private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Corrected the typo
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Signin from './pages/Signin';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Header from './components/Header';
+import PrivateRoute from './components/privateRoute';
+
 function App() {
   return (
-    <BrowserRouter> {/* Corrected component name */}
-    <Header/>
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import {useSelector}  from  'react-redux';
 function Header() {
+  const {currentUser} = useSelector((state)=>state.user);
+
   return (
     <div className=" text-black">
       <div className="flex justify-between items-center max-w-6xl mx-auto py-6 px-3">
@@ -15,8 +17,14 @@ function Header() {
           <Link to="/about" className="hover:underline">
             <li className="transition-all">About</li>
           </Link>
-          <Link to="/sign-up" className="hover:underline">
-            <li className="transition-all">SignUp</li>
+          
+          <Link to="/profile" className="hover:underline">
+          {currentUser? (
+            <img src={currentUser.profilePicture}alt='profile' className=' h-7 w-7 rounded-full object-cover'></img>
+          ):(
+   <li className="transition-all">Sign In</li>
+          )}
+            
           </Link>
         </ul>
       </div>
