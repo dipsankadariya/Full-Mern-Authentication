59ef9caf6ac13f70ce62634bd0b9099f4b057d5d
Dipsan Kadariya <dipsankadariya99@gmail.com>
2025-01-04 14:36:13 +0545

    Added  Profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="font-bold text-3xl text-center my-8">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex justify-center mb-4">
+          <img
+            src={currentUser.profilePicture || '/default-avatar.png'}
+            alt="Profile"
+            className="h-24 w-24 cursor-pointer rounded-full object-cover"
+          />
+        </div>
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-gray-200 rounded-lg p-3 text-left text-lg"
+        />
+        <input
+          defaultValue={currentUser.email}
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-gray-200 rounded-lg p-3 text-left text-lg"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-gray-200 rounded-lg p-3 text-left text-lg"
+        />
+        <button
+          type="submit"
+          className="bg-black py-3 text-white text-lg hover:opacity-70"
+        >
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-6 text-lg">
+        <button className="text-red-500 font-bold hover:opacity-70">
+          Delete Account
+        </button>
+        <button className="text-blue-500 font-bold hover:opacity-70">
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
