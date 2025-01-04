c7ec07ef7aeca5598bfdb64b0a0c95b2b2237726
Dipsan Kadariya <dipsankadariya99@gmail.com>
2025-01-04 22:24:57 +0545

    deployment adjustments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Signin from './pages/Signin';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Header from './components/Header';
-import PrivateRoute from './components/privateRoute';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
